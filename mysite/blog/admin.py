c79832317199aa779bc3d0b01e7b97ff0263b426
boonyong <boonyong.kang93@gmail.com>
2024-10-15 16:13:11 +0800

    Customize Post model view in admin site

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Post
 
+
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "publish", "status"]
+    list_filter = ["status", "created", "publish", "author"]
+    search_fields = ["title", "body"]  # Search bar
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ["author"]  # Lookup widget
+    date_hierarchy = "publish"  # Date breadcrumbs (below search bar)
+    ordering = ["status", "publish"]  # Overrides the default sort order of the model.
+    show_facets = admin.ShowFacets.ALWAYS  # Object counts for each filter.
