097a0489b23c7d6e5f1f16105f43fc1cf3373264
boonyong <boonyong.kang93@gmail.com>
2024-10-18 15:37:01 +0800

    Implement pagination - Add a generic pagination template. - `Post` list view.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,25 @@
-from django.http import Http404
+from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
 # Create your views here.
-def post_list(request):
-    posts = Post.published.all()
+def post_list(request: HttpRequest):
+    post_list = Post.published.all()
+    # Pagination with 3 posts per page.
+    paginator = Paginator(post_list, 3)
+    # Retrieve the HTTP `GET` parameter. (Default: 1st page)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # If page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # If page_number is not an integer get the first page.
+        posts = paginator.page(1)
+
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
