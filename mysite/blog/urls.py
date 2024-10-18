d45d0c6b345b13c804a839eefb7732c7dcfe6a77
boonyong <boonyong.kang93@gmail.com>
2024-10-18 12:36:40 +0800

    Implement SEO-friendly URL - `Post` detail view

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     # Post views
     path("", views.post_list, name="post_list"),
     # Use `<>` to capture values from the URL.
-    path("<int:id>/", views.post_detail, name="post_detail"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<slug:post>/",
+        views.post_detail,
+        name="post_detail",
+    ),
 ]
