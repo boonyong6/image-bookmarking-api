54ca973830a69218b40297149ebaf8cfacd56b91
boonyong <boonyong.kang93@gmail.com>
2024-10-24 18:52:43 +0800

    Query with weighting

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -155,13 +155,16 @@ def post_search(request: HttpRequest):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            search_vector = SearchVector("title", "body")
+            # Default weights are D, C, B, A (0.1, 0.2, 0.4, 1.0).
+            search_vector = SearchVector("title", weight="A") + SearchVector(
+                "body", weight="B"
+            )
             search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
                     search=search_vector, rank=SearchRank(search_vector, search_query)
                 )
-                .filter(search=search_query)
+                .filter(rank__gte=0.3)
                 .order_by("-rank")
             )
 
