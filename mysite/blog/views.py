7a6a119fcd4ede1b0f1c72c7082ac9abe1ae0251
boonyong <boonyong.kang93@gmail.com>
2024-10-24 17:25:37 +0800

    Implement search (powered by PostgreSQL) - Migrate to PostgreSQL. - Install PostgreSQL driver (`psycopg`). - Add post search page. - Search on multiple fields.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,9 +5,10 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 from taggit.models import Tag
 
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Comment, Post
 
 
@@ -142,4 +143,24 @@ def post_comment(request: HttpRequest, post_id):
         request,
         "blog/post/comment.html",
         {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request: HttpRequest):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body")
+            ).filter(search=query)
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )
