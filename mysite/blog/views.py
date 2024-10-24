11d0c2391bef101926a693b007811b8763c91375
boonyong <boonyong.kang93@gmail.com>
2024-10-24 18:15:56 +0800

    Order search results by relevancy - Using `SearchRank`

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from taggit.models import Tag
 
 from .forms import CommentForm, EmailPostForm, SearchForm
@@ -155,9 +155,15 @@ def post_search(request: HttpRequest):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            results = Post.published.annotate(
-                search=SearchVector("title", "body")
-            ).filter(search=query)
+            search_vector = SearchVector("title", "body")
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(
+                    search=search_vector, rank=SearchRank(search_vector, search_query)
+                )
+                .filter(search=search_query)
+                .order_by("-rank")
+            )
 
     return render(
         request,
