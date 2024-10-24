c7000b5544bf6bc4585e9063227262e081d28caa
boonyong <boonyong.kang93@gmail.com>
2024-10-24 21:14:13 +0800

    Add `Tag` sitemap

--- mysite/blog/sitemaps.py
+++ mysite/blog/sitemaps.py
@@ -1,4 +1,6 @@
 from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from taggit.models import Tag
 from .models import Post
 
 
@@ -11,3 +13,14 @@ class PostSitemap(Sitemap):
 
     def lastmod(self, obj: Post):
         return obj.updated
+
+
+class TagSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.6
+
+    def items(self):
+        return Tag.objects.all()
+
+    def location(self, obj: Tag):
+        return reverse("blog:post_list_by_tag", args=[obj.slug])
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,11 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
-from blog.sitemaps import PostSitemap
+from blog.sitemaps import PostSitemap, TagSitemap
 from . import views
 
 sitemaps = {
     "posts": PostSitemap,
+    "tags": TagSitemap,
 }
 
 urlpatterns = [
