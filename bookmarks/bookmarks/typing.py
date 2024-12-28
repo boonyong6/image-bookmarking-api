b8233d7788157f6a0fa8d14bcb451ed5e0449baa
boonyong <boonyong.kang93@gmail.com>
2024-12-28 21:18:56 +0800

    Store image views in Redis

--- bookmarks/bookmarks/typing.py
+++ bookmarks/bookmarks/typing.py
@@ -5,6 +5,9 @@ from django.conf import settings
 
 class _SettingsProtocol(Protocol):
     AUTH_USER_MODEL: str
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_DB: int
 
 
 settings = cast(_SettingsProtocol, settings)
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,4 @@
+import redis
 from actions.utils import create_action
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -7,8 +8,15 @@ from django.http.request import HttpRequest
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
 
+from bookmarks.typing import settings
+
 from .forms import ImageCreateForm
 from .models import Image
+
+# Connect to redis
+r = redis.Redis(
+    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+)
 
 
 @login_required
@@ -38,8 +46,12 @@ def image_create(request: HttpRequest):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # Increment total image views by 1.
+    total_views = r.incr(f"image:{image.id}:views")
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image, "total_views": total_views},
     )
 
 
