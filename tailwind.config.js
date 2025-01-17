85b6c93b2b82bddd05214e44ba4242f6015dbcb4
boonyong <boonyong.kang93@gmail.com>
2025-01-17 15:45:15 +0800

    Style more `allauth` templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*/templates/**/*.html"],
+  content: ["./accounts/templates/**/*.html"],
   theme: {
     extend: {},
   },
